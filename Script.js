b3e6cf2b35101d7831c1abc2bc0177bf93a3a0a9
phanikurapati <phanikurapati40@gmail.com>
2026-06-18 11:19:40 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+// ==========================
+// DOM ELEMENTS
+// ==========================
+
+const taskForm = document.getElementById("task-form");
+const taskInput = document.getElementById("task
